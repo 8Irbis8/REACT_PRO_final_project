87977a0e14f815abcd6fc3c477adfc1f84e5f7eb
8Irbis8 <it@8Irbis8.ru>
2025-11-16 19:17:33 +0300

    fix build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
 import { developmentConfig } from './vite/dev.config';
+import { productionConfig } from './vite/prod.config';
 
 // Алиасы для FSD архитектуры
 const FSD_ALIASES = {
